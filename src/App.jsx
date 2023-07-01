345db36bea2774318edd9e840435baccd0413feb
nehemiahcode <codehubby@gmail.com>
2023-07-01 11:36:30 +0100

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -36,21 +36,16 @@ function Catfact() {
         </div>
       ) : (
         <>
-          <h1 className=" font-bold text-4xl text-center dark:text-white pt-5">
+          <h1 className=" font-bold text-4xl text-center dark:text-white pt-5 bg-gradient-to-t from-slate-800 pb-2 ">
             Cat Facts
           </h1>
-          <div className="bg-black dark:bg-white rounded-lg w-[95%] lg:w-[70%]  mt-5 sm:w-[90%] h-[auto] lg:h-[600px] shadow-xl mx-auto  flex items-center flex-col py-5">
-            <button
-              onClick={fetchCatFact}
-              className="bg-slate-900 dark:bg-blue-900 px-4 py-4 dark:ring-lime-500 w-[200px] font-bold rounded-full text-white text-lg hover:ring-4 ring-inset ring-slate-600"
-            >
-              Generate Facts
-            </button>
+          <div className="bg-black dark:bg-white rounded-lg w-[95%] lg:w-[70%]  mt-5 sm:w-[90%] h-[auto] shadow-xl mx-auto  flex items-center flex-col py-5">
+           
             {catImage && (
               <img
                 src={catImage}
                 alt="Random Cat"
-                className="mx-auto mt-4 rounded-md w-[85%] sm:w-[70%] md:w-[50%] lg:w-[50%] lg:h-[230px] object-cover h-[190px]"
+                className="mx-auto mt-3 rounded-md w-[85%] sm:w-[70%] md:w-[50%] lg:w-[50%] lg:h-[230px] object-cover h-[190px]"
               />
             )}
             <div className=" bg-neutral-900 dark:bg-white shadow-2xl my-8 w-[85%] sm:w-[70%] lg:py-12 md:w-[50%] h-[auto] lg:h-[300px] rounded-lg px-7 py-10">
@@ -66,6 +61,12 @@ function Catfact() {
                 </strong>
               </p>
             </div>
+            <button
+              onClick={fetchCatFact}
+              className="bg-slate-700 dark:bg-blue-900 px-6 py-4 dark:ring-lime-500 w-[auto] shadow-lg font-bold rounded-full text-white text-lg hover:ring-lime-700 hover:ring-4 ring-inset ring-slate-600"
+            >
+              Generate More Facts
+            </button>
           </div>
         </>
       )}
