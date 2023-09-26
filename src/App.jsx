6299a4cd4f205199556d7e771894fbdb50ac8e11
nehemiahcode <codehubby@gmail.com>
2023-09-26 23:55:45 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Image,
+  Spinner,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { SlOptionsVertical } from "react-icons/sl";
 
 function Catfact() {
   const [catFact, setCatFact] = useState("");
@@ -32,7 +41,6 @@ function Catfact() {
     return Options.find((opt) => opt.text !== CurrentOption);
   };
 
-
   // Api used to get catfatcs
   const fetchCatFact = () => {
     axios.get("https://catfact.ninja/fact").then((res) => {
@@ -53,51 +61,62 @@ function Catfact() {
   useEffect(() => {
     const Timer = setTimeout(() => {
       setAnimation(false);
-    }, 1000);
+    }, 2000);
   });
   return (
     <>
-    {/* Animation before page loads */}
+      {/* Animation before page loads */}
       {animation ? (
         <div className=" fixed h-full w-full bg-white flex flex-col justify-center items-center">
-          <h1 className="  font-bold text-lg pb-10 ">
-            Fetching your Facts....
-          </h1>
-          <div className=" bg-black  w-20 h-20 rounded-full  animate-bounce   shadow-2xl flex justify-center items-center">
-            <div className=" bg-white w-10 h-10 rounded-full"></div>
-          </div>
+          <Spinner size="lg" />
         </div>
       ) : (
         <>
-        {/* header with dark mode icons */}
-          <header className=" bg-white dark:bg-slate-900 h-[60px] flex items-center justify-end px-5
-           shadow-2xl static top-0 left-0 w-[100%] z-50 ">
+          {/* header with dark mode icons */}
+          <header
+            className=" bg-white dark:bg-slate-900 h-[60px] flex items-center justify-end px-5
+           shadow-2xl static top-0 left-0 w-[100%] z-50 "
+          >
             <div
               onClick={() => setTheme(getOtherOption(theme).text)}
               className={` h-10 w-10 flex justify-center items-center text-2xl font-extrabold rounded-full
                text-black bg-white dark:bg-slate-700 dark:text-white`}
             >
-              <ion-icon
-                name={theme === "light" ? Options[0].icon : Options[1].icon}
-              ></ion-icon>
+              <Popover placement="right">
+                <PopoverTrigger>
+                  <Button isIconOnly>
+                    <SlOptionsVertical />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className=" py-4 px-4 w-[100px] bg-slate-900 dark:bg-white dark:text-black text-white">
+                  <ion-icon
+                    name={theme === "light" ? Options[0].icon : Options[1].icon}
+                  ></ion-icon>
+                </PopoverContent>
+              </Popover>
             </div>
           </header>
           <h1 className=" font-bold text-4xl text-center text-black dark:text-white pt-5  pb-2 ">
             Cat Facts
           </h1>
           {/* Main section with catfacts images and facts */}
-          <div className=" dark:bg-slate-900 bg-white rounded-lg w-[95%] lg:w-[70%]  
-          mt-5 sm:w-[90%] h-[auto] lg:h-[100%] shadow-xl mx-auto  flex items-center flex-col py-5">
+          <div
+            className=" dark:bg-slate-900 bg-white rounded-lg w-[95%] lg:w-[70%]  
+          mt-5 sm:w-[90%] h-[auto] lg:h-[100%] shadow-xl mx-auto  flex items-center flex-col py-5"
+          >
             {catImage && (
-              <img
+              <Image
                 src={catImage}
-                alt="Random catfact images.jpg"
-                className="mx-auto mt-3 rounded-md w-[85%] sm:w-[70%] md:w-[50%] lg:w-[50%] lg:h-[230px] object-cover h-[190px]"
+                alt="Random catfact auto generated images"
+                loading="lazy"
+                className="mx-auto mt-3 rounded-md w-[100%]  object-cover h-[190px]"
               />
             )}
-            <div className=" dark:bg-slate-900 bg-white shadow-2xl my-8 w-[85%] sm:w-[70%] lg:py-12
-             md:w-[50%] h-[auto] lg:h-[300px] rounded-lg px-7 py-10">
-              <p className="dark:text-white text-black font-medium text-lg text-center">
+            <div
+              className=" dark:bg-slate-900 bg-white shadow-2xl my-8 w-[85%] sm:w-[70%] lg:py-12
+             md:w-[50%] h-[auto] lg:h-[300px] rounded-lg px-7 py-10"
+            >
+              <p className="dark:text-white text-black font-medium text-center">
                 <strong className=" font-extrabold dark:text-white text-red-600 text-3xl">
                   {" "}
                   "{" "}
@@ -110,15 +129,14 @@ function Catfact() {
               </p>
             </div>
             {/* button used to generate facts */}
-            <button
+            <Button
               onClick={fetchCatFact}
               className="bg-slate-900
-                 border-b-[7px] border-blue-700 focus:border-0
-                dark:bg-blue-900 px-6 py-4 overflow-hidden dark:ring-lime-500 w-[auto] shadow-lg font-bold 
+                dark:bg-blue-900 px-6 py-6  dark:ring-lime-500 w-[auto] shadow-lg font-medium 
                 rounded-full text-white text-lg  ring-inset ring-slate-600"
             >
-              Generate More Facts
-            </button>
+              Generate Facts
+            </Button>
           </div>
         </>
       )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+const { nextui } = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode:"class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     screens: {
@@ -24,6 +26,6 @@ export default {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
 }
 
